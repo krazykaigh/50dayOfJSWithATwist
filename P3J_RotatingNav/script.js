992f96c45984cbb8ef1d5ed3f199716b80caefa8
Kaigh Taylor <coders@kai-deas.com>
2021-05-06 20:13:00 -0400

    50DysWTwist | P3 Update

--- P3J_RotatingNav/script.js
+++ P3J_RotatingNav/script.js
@@ -1,6 +1,6 @@
 const open = document.getElementById('open');
-const closed = document.getElementById('closed');
+const close = document.getElementById('close');
 const container = document.querySelector('.container');
 
 open.addEventListener('click', () => container.classList.add('show-nav'));
-
+close.addEventListener('click', () => container.classList.remove('show-nav'));
