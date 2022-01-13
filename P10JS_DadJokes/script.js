cb1c7783369def508acd18f4d9cac62a6069a689
Kaigh Taylor <coders@kai-deas.com>
2022-01-13 15:09:52 -0500

    P10JS_dadJokes | jshint setup

--- P10JS_DadJokes/script.js
+++ P10JS_DadJokes/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 const jokeEL = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
@@ -6,35 +8,33 @@ jokeBtn.addEventListener('click', generateJoke());
 generateJoke();
 
 // USING async await
-// async function generateJoke() {
-//     const config = {
-//         headers: {
-//             'Accept': 'application/json'
-//         },
-//     }
-
-//     const res = await fetch('https://icanhazdadjoke.com', 
-//     config)
-
-//     const data = await res.json()
-
-//     jokeEL.innerHTML = data.joke
-
-// }
-
-
-USING .then()
-function generateJoke() {
+async function generateJoke() {
     const config = {
         headers: {
             'Accept': 'application/json'
-        }
-    }
+        },
+    };
+    const res = await fetch('https://icanhazdadjoke.com',
+        config);
 
-    fetch('https://icanhazdadjoke.com', config)
-        .then((res) => res.json())
-        .then((data) => {
-            jokeEL.innerHTML = data.joke
-        })
-        //console.log(data))
+    const data = await res.json();
+
+    jokeEL.innerHTML = data.joke;
+
 }
+
+// USING .then()
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEL.innerHTML = data.joke
+//         })
+//         //console.log(data))
+// }
