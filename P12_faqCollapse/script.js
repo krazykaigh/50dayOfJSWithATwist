b963ff361fa9bf44254ceef767d2ceef0bb57489
Kaigh Taylor <coders@kai-deas.com>
2022-04-22 13:30:45 -0400

    P12_FaqCollapse | jshint

--- P12_faqCollapse/script.js
+++ P12_faqCollapse/script.js
@@ -1,7 +1,8 @@
+/*jshint esversion: 8 */
 const toggles = document.querySelectorAll('.faq-toggle')
 
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
-    })
-})
+    });
+});
